f51f1d808296d2657107a87585e5098726b231ab
DiegoBV <diego.baratto8@gmail.com>
2018-03-01 21:07:54 +0100

    DSI updateado

--- DSI y tal/Project3/Source.cpp
+++ DSI y tal/Project3/Source.cpp
@@ -2,6 +2,8 @@
 #include <d2d1.h>
 #pragma comment(lib, "d2d1")
 #include "basewin.h"
+#include <windowsx.h>
+
 template <class T> void SafeRelease(T **ppT)
 {
 	if (*ppT)
@@ -10,14 +12,69 @@ template <class T> void SafeRelease(T **ppT)
 		*ppT = NULL;
 	}
 }
+
+class DPIScale
+{
+	static float scaleX;
+	static float scaleY;
+public:
+	static void Initialize(ID2D1Factory *pFactory)
+	{
+		FLOAT dpiX, dpiY;
+		pFactory->GetDesktopDpi(&dpiX, &dpiY);
+		scaleX = dpiX / 96.0f;
+		scaleY = dpiY / 96.0f;
+	}
+	template <typename T>
+	static D2D1_POINT_2F PixelsToDips(T x, T y)
+	{
+		return D2D1::Point2F(static_cast<float>(x) / scaleX,
+			static_cast<float>(y) / scaleY);
+	}
+};
+
+float DPIScale::scaleX = 1.0f;
+float DPIScale::scaleY = 1.0f;
+
+class MouseTrackEvents
+{
+	bool m_bMouseTracking;
+public:
+	MouseTrackEvents() : m_bMouseTracking(false)
+	{
+	}
+
+	void OnMouseMove(HWND hwnd)
+	{
+		if (!m_bMouseTracking)
+		{
+			// Enable mouse tracking.
+			TRACKMOUSEEVENT tme;
+			tme.cbSize = sizeof(tme);
+			tme.hwndTrack = hwnd;
+			tme.dwFlags = TME_HOVER | TME_LEAVE;
+			tme.dwHoverTime = HOVER_DEFAULT;
+			TrackMouseEvent(&tme);
+			m_bMouseTracking = true;
+		}
+	}
+	void Reset(HWND hwnd)
+	{
+		m_bMouseTracking = false;
+	}
+};
+
 class MainWindow : public BaseWindow<MainWindow>
 {
 	ID2D1Factory *pFactory;
 	ID2D1HwndRenderTarget *pRenderTarget;
 	ID2D1SolidColorBrush *pBrush;
 	D2D1_ELLIPSE ellipse;
+	D2D1_ELLIPSE newEllipse;
 	ID2D1SolidColorBrush *pStroke;
 	ID2D1SolidColorBrush *pFill;
+	D2D1_POINT_2F ptMouse;
+	MouseTrackEvents mouseTrack;
 	double IDT_TIMER;
 	TIMERPROC MyTimerProc;
 	void CalculateLayout();
@@ -25,8 +82,11 @@ class MainWindow : public BaseWindow<MainWindow>
 	void DiscardGraphicsResources();
 	void OnPaint();
 	void Resize();
+
 public:
-	MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL), pStroke(NULL), pFill(NULL)
+	MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL),
+		ellipse(D2D1::Ellipse(D2D1::Point2F(), 0, 0)),
+		ptMouse(D2D1::Point2F())
 	{
 	}
 	PCWSTR ClassName() const { return L"Circle Window Class"; }
@@ -34,7 +94,12 @@ public:
 	void MainWindow::DrawClockHand(float fHandLength, float fAngle, float
 		fStrokeWidth);
 	void MainWindow::RenderScene();
+	void OnLButtonDown(int pixelX, int pixelY, DWORD flags);
+	void OnLButtonUp();
+	void OnMouseMove(int pixelX, int pixelY, DWORD flags);
+
 };
+
 // Recalculate drawing layout when the size of the window changes.
 
 void MainWindow::DrawClockHand(float fHandLength, float fAngle, float
@@ -56,8 +121,10 @@ void MainWindow::DrawClockHand(float fHandLength, float fAngle, float
 void MainWindow::RenderScene()
 {
 	pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
-	pRenderTarget->FillEllipse(ellipse, pFill);
+	pRenderTarget->FillEllipse(ellipse, pBrush);
 	pRenderTarget->DrawEllipse(ellipse, pStroke);
+	pRenderTarget->FillEllipse(newEllipse, pFill);
+	pRenderTarget->DrawEllipse(newEllipse, pStroke);
 	// Draw hands
 	SYSTEMTIME time;
 	GetLocalTime(&time);
@@ -82,7 +149,7 @@ void MainWindow::CalculateLayout()
 		const float x = size.width / 2;
 		const float y = size.height / 2;
 		const float radius = min(x, y);
-		ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+		ellipse = D2D1::Ellipse(D2D1::Point2F(x/8, y/8), radius/8, radius/8);
 	}
 }
 HRESULT MainWindow::CreateGraphicsResources()
@@ -174,6 +241,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	return 0;
 }
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -186,6 +254,7 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			return -1; // Fail CreateWindowEx.
 		}
+		DPIScale::Initialize(pFactory);
 		return 0;
 	case WM_DESTROY:
 		DiscardGraphicsResources();
@@ -201,6 +270,80 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_SIZE:
 		Resize();
 		return 0;
+
+	case WM_LBUTTONDOWN :
+		{
+			POINT pt = { GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam) };
+			/*if (pt.x < newEllipse.point.x - newEllipse.radiusX && pt.x > newEllipse.point.x + newEllipse.radiusX && pt.y < newEllipse.point.y - newEllipse.radiusY && pt.y > newEllipse.point.y + newEllipse.radiusY) {
+				
+				newEllipse.point = D2D1::Point2F(pt.x, pt.y);
+			}
+			else*/ if (DragDetect(m_hwnd, pt))
+			{
+				OnLButtonDown(pt.x, pt.y, (DWORD)wParam);
+			}
+		}
+		return 0;
+
+	case WM_LBUTTONUP:
+		OnLButtonUp();
+		return 0;
+
+	case WM_MOUSEMOVE:
+		OnMouseMove(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), (DWORD)wParam);
+		mouseTrack.OnMouseMove(m_hwnd);
+		return 0;
+
+	case WM_MOUSELEAVE:
+		// TODO: Handle the mouse-leave message.
+		mouseTrack.Reset(m_hwnd);
+		return 0;
+
+	case WM_MOUSEHOVER:
+		// TODO: Handle the mouse-hover message.
+		mouseTrack.Reset(m_hwnd);
+		return 0;
 	}
+
 	return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+}
+
+void MainWindow::OnLButtonDown(int pixelX, int pixelY, DWORD flags)
+{
+	SetCapture(m_hwnd);
+	newEllipse.point = ptMouse = DPIScale::PixelsToDips(pixelX, pixelY);
+	newEllipse.radiusX = newEllipse.radiusY = 1.0f;
+	InvalidateRect(m_hwnd, NULL, FALSE);
+}
+
+void MainWindow::OnMouseMove(int pixelX, int pixelY, DWORD flags)
+{
+	if (flags & MK_LBUTTON)
+	{
+		const D2D1_POINT_2F dips = DPIScale::PixelsToDips(pixelX, pixelY);
+		const float width = (dips.x - ptMouse.x) / 2;
+		const float height = (dips.y - ptMouse.y) / 2;
+		const float x1 = ptMouse.x + width;
+		const float y1 = ptMouse.y + height;
+		newEllipse = D2D1::Ellipse(D2D1::Point2F(x1, y1), width, height);
+		InvalidateRect(m_hwnd, NULL, FALSE);
+	}
+}
+
+void MainWindow::OnLButtonUp()
+{
+	ReleaseCapture();
+}
+
+UINT GetMouseHoverTime()
+{
+	UINT msec;
+	if (SystemParametersInfo(SPI_GETMOUSEHOVERTIME, 0, &msec, 0))
+	{
+		return msec;
+	}
+	else
+	{
+		return 0;
+	}
 }
