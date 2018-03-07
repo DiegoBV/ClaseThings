c9fa8e6c7ffbe7051d0f8b47ce28a92195c9a70c
Diego BV <diego.baratto8@gmail.com>
2018-03-07 10:30:47 +0100

    gitignore

---
+++ ProyectosSDL/HolaSDL/StarTrekBulletsManager.cpp
@@ -0,0 +1,91 @@
+#include "StarTrekBulletsManager.h"
+#include "FillRectRenderer.h"
+#include "BasicMotionPhysics.h"
+#include "ExampleGame.h"
+
+
+
+
+StarTrekBulletsManager::StarTrekBulletsManager()
+{
+}
+
+
+StarTrekBulletsManager::~StarTrekBulletsManager()
+{
+}
+
+void StarTrekBulletsManager::shoot(Fighter* owner, Vector2D p, Vector2D v) { //Comprueba el estado actual de las balas (Hay alguna inactiva?)
+	/*pair<bool, Bullets*> check = bullets_.getObjectPool();
+
+	if (check.first) { //Si hay alguna inactiva, la activa
+		check.second->setActive(true);
+		check.second->setPosition(p);
+		check.second->setVelocity(v);
+	}
+	else { //Si no, crea una nueva
+		newShoot(check.second, v, p);
+	}*/
+}
+
+void StarTrekBulletsManager::update(Uint32 time) { //Esto sólo actualiza el estado de las balas
+	for (int i = 0; i < bullets_.size(); i++) {
+		if ((*bullets_.getItem(i))->isActive()) {
+			static_cast<Bullets*>(*bullets_.getItem(i))->update(time);
+		}
+	}
+}
+
+pair<bool, Bullets*> StarTrekBulletsManager::checkBullets() {
+	Uint32 i = 0;
+	bool encontrado = false;
+	Bullets* aux;
+	while (i < bullets_.size()) {
+		if (!(*bullets_.getItem(i))->isActive()) {
+			encontrado = true;
+			aux = (*bullets_.getItem(i));
+		}
+		i++;
+	}
+	pair<bool, Bullets*> resultado(encontrado, aux);
+	return resultado;
+}
+
+void StarTrekBulletsManager::render(Uint32 time) {
+	for (int i = 0; i < bullets_.size(); i++) {
+		static_cast<Bullets*>(*bullets_.getItem(i))->render(time);
+	}
+}
+
+void StarTrekBulletsManager::newShoot(Bullets* bull, Vector2D vel, Vector2D pos) {
+	//Creamos los metodos basicos de render y fisicas
+	FillRectRenderer* fillRect = new FillRectRenderer();
+	BasicMotionPhysics* motion = new BasicMotionPhysics();
+
+	//añadimos dichos metodos al Container
+	bull->addPhysicsComponent(motion);
+	bull->addRenderComponent(fillRect);
+
+	//establecemos su tamaño y lo añadimos a la lista de balas
+	bull->setHeight(5);
+	bull->setWidth(5);
+	bull->setVelocity(vel);
+	bull->setPosition(pos);
+	//bullets.addNewItem(newBullet);
+	//static_cast<ExampleGame*>(game)->pushObject(bull);
+}
+
+void StarTrekBulletsManager::receive(Message* msg) {
+	switch (msg->id_) {
+	case ROUND_START:
+		break;
+	case ROUND_END:
+		break;
+	case BULLET_ASTEROID_COLISION:
+		break;
+	case BULLET_FIGHTER_COLLISION:
+		break;
+	case FIGHTER_SHOOT:
+		break;
+	}
+}
---
+++ ProyectosSDL/HolaSDL/Weapon.cpp
@@ -0,0 +1,49 @@
+#include "Weapon.h"
+
+Weapon::~Weapon()
+{
+}
+
+void Weapon::handleInput(GameObject* o, Uint32 time, const SDL_Event& event) {
+	auxTime++; //aumento de la variable que controla el intervalo de tiempo
+	if (event.type == SDL_KEYDOWN) {
+		if (event.key.keysym.sym == disparo) {
+			if(puedeDisparar())
+				shoot(o);
+		}
+	}
+}
+
+bool Weapon::puedeDisparar() {
+	if (auxTime > timeInterval) { //ha pasado un inteervalo de tiempo
+		auxShots = 0;
+		auxTime = 0; //reinicio variables, devuelvo false
+		return false;
+	}
+	else if (auxShots >= shotsPerInterval) { //si el intervalo no ha pasado y se pasa de disparos, devuelve false
+		return false;
+	}
+	else { //si no ha pasado el intervalo de tiempo y puede seguir disparando, sumo variables y devuelvo true
+		auxShots++;
+		return true;
+	}
+		
+}
+
+void Weapon::shoot(GameObject* o) {
+	//Calculo de posicion
+	Vector2D aux = o->getPosition();
+	int x = aux.getX() + o->getWidth() / 2;
+	int y = aux.getY() + o->getHeight() / 2;
+	Vector2D dirAct = o->getDirection();
+	aux.setX(x);
+	aux.setY(y);
+	dirAct.setX(-dirAct.getX());
+	aux = aux + dirAct * (o->getHeight() / 2);
+
+	//calculo de velocidad
+	Vector2D vel = dirAct * max(o->getVelocity().magnitude() * 1.5, 2.0);
+
+	//shoot
+	//stw->shoot(aux, vel);
+}
