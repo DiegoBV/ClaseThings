b5fd12f1043499dc68dbf412c28163ebc02dc5eb
Diego BV <diego.baratto8@gmail.com>
2018-04-27 10:30:50 +0200

    Labos26Abril

--- PracticaInterfaces/RumbleController/BaseHID.cpp
+++ PracticaInterfaces/RumbleController/BaseHID.cpp
@@ -9,6 +9,38 @@ void BaseHID::Actualiza()
 		Mando2HID(); //Vuelco de Mando a HID normalizando
 					 //Actualizo Gestos de entrada genéricos (entradas)
 					 //Genero Gesto de feedback (salida)
-		EscribeMando(); //Escribo en Mando el feedback
+
+		wButtonsDown = (-wLastButtons) & (wButtons);
+		wButtonsUp = (wLastButtons) & (-wButtons);
+
+		fThumbLXf = (1 - a) + fThumbLXf + a*fThumbLX;
+		fThumbLYf = (1 - a) + fThumbLYf + a*fThumbLY;
+
+		fThumbRXf = (1 - a) + fThumbRXf + a*fThumbRX;
+		fThumbRYf = (1 - a) + fThumbRYf + a*fThumbRY;
+
+		if ((fThumbLX > 0) & (fThumbLY > 0)) {
+			roState = pp;
+			tRo = 1.0;
+		}
+
+		if (tRo > 0.0) {
+			tRo = tRo - T;
+			if (roState == np) roState = pp;
+			if ((fThumbLX > 0) & (fThumbLY <= 0) & (roState == pp)) roState = pn;
+
+			if ((fThumbLX <= 0) & (fThumbLY < 0) & (roState == pn)) roState = nn;
+
+			if ((fThumbLX < 0) & (fThumbLY >= 0) & (roState == nn)) roState = np;
+		}
+		else
+			tRo = pp;
+
+		if (tLR > 0) tLR -= T;
+		else sLR(0.0, 0.0);
+		if (tRR > 0) tRR -= T;
+		else sRR(0.0, 0.0);
+
+		EscribeMando();
 	}
 }
--- PracticaInterfaces/RumbleController/BaseHID.h
+++ PracticaInterfaces/RumbleController/BaseHID.h
@@ -67,16 +67,4 @@ public:
 	void Actualiza(); //Actualiza Mando2HID y HID2Mando.
 
 };
-void BaseHID::Actualiza()
-{
-	wLastButtons = wButtons; //Copia estado de botones
-	bConected = LeeMando(); //Leo Mando
-	if (bConected == true)
-	{
-		Mando2HID(); //Vuelco de Mando a HID normalizando
-					 //Actualizo Gestos de entrada genéricos (entradas)
-			//Genero Gesto de feedback (salida)
-			EscribeMando(); //Escribo en Mando el feedback
-	}
-}
 
